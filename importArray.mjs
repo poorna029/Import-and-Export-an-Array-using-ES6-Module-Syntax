d722df838be933d4bc00224b1d9e6ca0ab7a049e
poorna <poorna029@gmail.com>
2022-11-14 07:14:05 +0000

    array is added

--- importArray.mjs
+++ importArray.mjs
@@ -1,3 +1,7 @@
 import array from "./exportArray.mjs";
 // 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
 // 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
+// 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
+// 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
+// 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
+console.log(array);
